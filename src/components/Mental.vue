24e54b3f58963bdf5e401b990580c09cb0cbd6b8
owen <owen@venmac.local>
2020-08-23 12:08:25 +0200

    settings

--- src/components/Mental.vue
+++ src/components/Mental.vue
@@ -1,5 +1,110 @@
 <template>
   <div class="hello">
+    <div v-if="!showSettings" @click="showSettings = true" class="settings-button">
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve">
+        <g>
+          <g>
+            <path d="M509.952,223.895c-1.685-12.864-8.96-24.021-19.989-30.677c-10.859-6.549-23.872-7.723-36.011-3.2
+              c-9.813,3.712-21.077-1.387-25.6-11.499c-5.205-11.605-11.584-22.72-19.008-33.045c-6.613-9.173-5.376-21.205,2.859-27.989
+              c9.813-8.064,15.296-19.989,15.04-32.683c-0.256-12.907-6.357-24.789-16.683-32.64c-17.024-12.907-35.584-23.616-55.189-31.872
+              c-12.053-5.077-25.429-4.373-36.736,1.856c-11.115,6.144-18.667,16.811-20.757,29.333c-1.771,10.56-11.541,17.621-22.933,16.448
+              c-12.459-1.323-25.429-1.323-37.867,0c-11.285,1.195-21.163-5.888-22.933-16.448c-2.091-12.523-9.643-23.211-20.757-29.333
+              c-11.328-6.251-24.725-6.933-36.736-1.856c-19.627,8.256-38.187,18.965-55.211,31.872C91.115,60.012,85.035,71.895,84.779,84.78
+              c-0.256,12.715,5.227,24.619,15.061,32.747c8.213,6.763,9.451,18.795,2.859,27.968c-7.445,10.325-13.824,21.44-19.029,33.045
+              c-4.501,10.133-15.787,15.211-25.835,11.413c-11.861-4.416-24.896-3.243-35.776,3.285c-11.051,6.635-18.325,17.813-19.989,30.677
+              C0.704,234.497,0,245.313,0,256.001c0,10.688,0.704,21.504,2.048,32.107c1.685,12.864,8.96,24.021,19.989,30.677
+              c10.859,6.528,23.851,7.723,36.011,3.2c9.792-3.733,21.056,1.365,25.6,11.499c5.205,11.605,11.584,22.72,19.008,33.045
+              c6.613,9.173,5.376,21.205-2.859,27.989c-9.813,8.064-15.296,19.989-15.04,32.683c0.256,12.907,6.357,24.789,16.683,32.64
+              c17.024,12.907,35.584,23.616,55.189,31.872c12.053,5.077,25.408,4.373,36.736-1.856c11.115-6.144,18.667-16.811,20.757-29.333
+              c1.771-10.56,11.627-17.621,22.933-16.448c12.437,1.323,25.408,1.323,37.867,0c11.413-1.131,21.163,5.888,22.933,16.448
+              c2.091,12.523,9.643,23.211,20.757,29.333c6.315,3.477,13.227,5.227,20.224,5.227c5.589,0,11.179-1.131,16.491-3.371
+              c19.605-8.256,38.165-18.987,55.189-31.872c10.347-7.829,16.427-19.733,16.683-32.619c0.256-12.715-5.227-24.64-15.04-32.725
+              c-8.213-6.763-9.451-18.795-2.859-27.968c7.445-10.325,13.824-21.44,19.029-33.045c4.523-10.133,15.829-15.189,25.6-11.499
+              l0.235,0.085c11.904,4.459,24.917,3.243,35.776-3.285c11.051-6.656,18.325-17.813,19.989-30.699
+              c1.365-10.688,2.069-21.461,2.069-32.085C512,245.377,511.296,234.604,509.952,223.895z M488.768,285.377
+              c-0.811,6.336-4.395,11.861-9.813,15.125c-5.227,3.179-11.541,3.733-17.515,1.493l-0.128-0.043
+              c-20.352-7.531-43.328,2.475-52.437,22.827c-4.608,10.283-10.283,20.16-16.875,29.312c-13.248,18.432-10.389,42.901,6.635,56.896
+              c4.736,3.904,7.381,9.664,7.253,15.808c-0.128,6.336-3.115,12.181-8.235,16.064c-15.573,11.819-32.597,21.653-50.581,29.205
+              c-5.952,2.517-12.608,2.176-18.155-0.875c-5.355-2.965-9.003-8.107-10.005-14.144c-3.392-20.288-20.757-34.411-41.387-34.411
+              c-1.579,0-3.2,0.064-4.821,0.192c-10.987,1.152-22.421,1.152-33.408,0c-22.699-2.581-42.56,12.309-46.208,34.155
+              c-1.003,6.037-4.651,11.2-10.005,14.144c-5.568,3.072-12.203,3.392-18.176,0.875c-17.984-7.573-35.008-17.387-50.581-29.205
+              c-5.12-3.861-8.107-9.728-8.235-16.043c-0.128-6.144,2.517-11.904,7.275-15.808c17.024-13.995,19.861-38.464,6.613-56.896
+              c-6.592-9.173-12.245-19.029-16.853-29.312c-9.131-20.395-32.192-30.357-52.821-22.677c-5.696,2.133-12.032,1.6-17.259-1.579
+              c-5.419-3.243-9.003-8.768-9.813-15.125c-1.259-9.685-1.899-19.584-1.899-29.355c0-9.771,0.64-19.669,1.92-29.376
+              c0.811-6.336,4.395-11.861,9.813-15.125c5.205-3.157,11.541-3.733,17.493-1.493c20.373,7.595,43.435-2.368,52.565-22.784
+              c4.608-10.283,10.283-20.139,16.875-29.291c13.248-18.432,10.389-42.901-6.635-56.896c-4.736-3.904-7.381-9.664-7.253-15.808
+              c0.128-6.336,3.115-12.181,8.235-16.064c15.573-11.819,32.597-21.632,50.581-29.205c5.952-2.517,12.587-2.197,18.155,0.875
+              c5.355,2.944,9.003,8.107,10.005,14.165c3.648,21.845,23.509,36.587,46.208,34.155c10.965-1.152,22.4-1.152,33.408,0
+              c22.571,2.411,42.56-12.309,46.208-34.155c1.003-6.037,4.651-11.2,10.005-14.144c5.568-3.093,12.181-3.413,18.176-0.875
+              c17.984,7.573,35.008,17.387,50.581,29.205c5.12,3.861,8.107,9.728,8.235,16.043c0.128,6.144-2.517,11.904-7.275,15.808
+              c-17.024,13.995-19.861,38.464-6.613,56.896c6.592,9.173,12.245,19.029,16.853,29.312c9.131,20.395,32.213,30.379,52.821,22.677
+              c5.717-2.155,12.011-1.579,17.259,1.579c5.419,3.264,9.003,8.789,9.813,15.125c1.259,9.792,1.899,19.669,1.899,29.376
+              C490.667,265.708,490.027,275.585,488.768,285.377z"/>
+          </g>
+        </g>
+        <g>
+          <g>
+            <path d="M256,149.335c-58.816,0-106.667,47.851-106.667,106.667S197.184,362.668,256,362.668s106.667-47.851,106.667-106.667
+              S314.816,149.335,256,149.335z M256,341.335c-47.061,0-85.333-38.272-85.333-85.333s38.272-85.333,85.333-85.333
+              s85.333,38.272,85.333,85.333S303.061,341.335,256,341.335z"/>
+          </g>
+        </g>
+      </svg>
+    </div>
+    <div class="settings-button" @click="showSettings = false" v-else>
+      <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+        viewBox="0 0 512 512.001" style="enable-background:new 0 0 512 512.001;" xml:space="preserve">
+      <g>
+        <g>
+          <path d="M284.284,256L506.142,34.142c7.811-7.81,7.811-20.474,0-28.284c-7.811-7.811-20.474-7.811-28.284,0L256,227.716
+            L34.142,5.858c-7.811-7.811-20.474-7.811-28.284,0c-7.811,7.81-7.811,20.474,0,28.284L227.716,256L5.858,477.858
+            c-7.811,7.811-7.811,20.474,0,28.284c7.81,7.81,20.473,7.811,28.284,0L256,284.284l221.858,221.858
+            c7.81,7.81,20.473,7.811,28.284,0c7.811-7.811,7.811-20.474,0-28.284L284.284,256z"/>
+        </g>
+      </g>
+      </svg>
+    </div>
+    <div v-if="showSettings" class="settings-wrapper">
+      <div class="settings">
+        <div>
+          <input v-model="operations.times" type="checkbox"/>
+          <label>Multiplications</label>
+        </div>
+        <div>
+          <input v-model="operations.plus" type="checkbox"/>
+          <label>Additions</label>
+        </div>
+        <div>
+          <input v-model="operations.minus" type="checkbox"/>
+          <label>Soustractions</label>
+        </div>
+        <div class="form-text">
+          <label>
+            Nombre maximum
+          </label>
+          <input
+          v-model.number="maxNumber"
+          type="number"
+          placeholder="Nombre maximum"
+          />
+        </div>
+        <div class="form-text">
+          <label>
+            Nombre minimum
+          </label>
+          <input
+          v-model.number="minNumber"
+          type="number"
+          placeholder="Nombre minimum"
+          />
+        </div>
+        <div class="form-button">
+          <button @click="showSettings = false" class="btn">Sauvegarder</button>
+        </div>
+      </div>
+    </div>
+
     <h1 :style="{fontSize}">
       {{mental.text}}
     </h1>
@@ -11,7 +116,7 @@
     @keydown.enter="validate"
     :class="{'win': winned === 1, 'loose': winned === -1}"
     type="number"
-    placeholder="equal..."
+    placeholder="égale..."
     />
 
     <div id="controls">
@@ -65,34 +170,34 @@
 
     <div class="states">
       <div>
-        level: <b>{{level - 1}}</b>
+        niveau: <b>{{level - 1}}</b>
       </div>
       <div>
-        wins: <b>{{totalWins}}</b>
+        victoires: <b>{{totalWins}}</b>
       </div>
       <div>
-        fails: <b>{{totalFails}}</b>
+        erreurs: <b>{{totalFails}}</b>
       </div>
     </div>
 
     <div v-for="(difficulty, index) in historyLevels" :key="index" class="history">
       <div v-if="history[difficulty] && difficulty" class="history-container">
-        <h1>stats for level {{ difficulty }}</h1>
+        <h1>Stats du niveau {{ difficulty }}</h1>
         <div class="table-container">
           <table class="history-table">
             <thead>
               <tr class="history-item-header">
                 <th class="history-item-text">
-                  calculation
+                  Calcul
                 </th>
                 <th class="history-item-fails">
-                  fails
+                  Erreurs
                 </th>
                 <th class="history-item-time">
-                  total time (s)
+                  Temps total (en secondes)
                 </th>
                 <th class="history-item-time">
-                  thinking time (s)
+                  Temps de réflexion (en secondes)
                 </th>
               </tr>
             </thead>
@@ -118,26 +223,26 @@
         </div>
         <div v-if="historyTimeAverage[difficulty].time > 0" class="states">
           <div>
-            average time: <b>{{historyTimeAverage[difficulty].time.toFixed(2)}}</b>
+            Temps total moyen: <b>{{historyTimeAverage[difficulty].time.toFixed(2)}}</b>
           </div>
           <div>
-            average thinking time: <b>{{historyTimeAverage[difficulty].thinkingTime.toFixed(2)}}</b>
+            Temps de réflexion moyen: <b>{{historyTimeAverage[difficulty].thinkingTime.toFixed(2)}}</b>
           </div>
         </div>
       </div>
     </div>
     <div v-if="historyLevels.length > 0" class="clear">
-      <button @click="clearHistory" class="btn">clear history</button>
+      <button @click="clearHistory" class="btn">Supprimer l'historique de calcul</button>
     </div>
     <div class="note">
       <div v-if="historyLevels.length > 0" class="italic">
-        <div class="indicator"/>this dot indicates your difficulty on a calculation (color intensity)
+        <div class="indicator"/>Ce point indique ta difficulté pour ce calcul (plus c'est rouge, plus tu as mis du temps)
       </div>
       <div v-if="historyLevels.length > 0" class="italic">
-        <div class="indicator give-up"/>this dot indicates an abandoned calculation
+        <div class="indicator give-up"/>Ce point indique que tu as abandonné ce calcul
       </div>
       <div v-if="historyLevels.length > 0" class="italic">
-        thinking time is the respone time without the typing time
+        Le temps de réflexion est le temps que tu as mis avant de tapper la réponse sur ton clavier
       </div>
     </div>
   </div>
@@ -178,7 +283,20 @@ export default {
       isTyping: false,
       typingWait: false,
       typingTimeTemp: 0,
-      fontSize: '2em'
+      fontSize: '2em',
+      minLevel: 0,
+      minNumber: 1,
+      showSettings: false,
+      operations: {
+        minus: true,
+        plus: true,
+        times: true
+      }
+    }
+  },
+  watch: {
+    showSettings() {
+      this.regenerate() 
     }
   },
   methods: {
@@ -232,7 +350,6 @@ export default {
         this.totalWins++
         this.fails = 0
         this.wins++
-        this.maxNumber += 2
         this.winned = 1
         this.generate()
         // if (this.wins >= this.maxWins) {
@@ -245,7 +362,6 @@ export default {
         this.totalFails++
         this.wins = 0
         this.fails++
-        this.maxNumber > 10 && --this.maxNumber
         this.winned = -1
         // if (this.fails >= this.maxFails) {
         //   this.fails = 0
@@ -283,7 +399,7 @@ export default {
       this.timer = setInterval(() => {
         this.actualTime += 0.1
       }, 100)
-      this.mental = generator(this.level, this.maxNumber)
+      this.mental = generator(this.level, this.maxNumber, this.minNumber, this.operations)
       this.setFontSize()
     },
     sort(a, b) {
@@ -384,6 +500,54 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.settings-button
+  width: 40px
+  position fixed
+  top 20px
+  right 20px
+  opacity 0.5
+  cursor pointer
+  z-index 9999
+
+input[type="checkbox"]
+  height 20px
+  width 20px
+  margin-right 10px
+
+label
+  vertical-align top
+  font-size 1.3rem
+  font-weight bold
+  text-align left
+  width 100%
+
+.form-button
+  margin-top 50px
+
+.settings
+  display flex
+  flex-direction column
+  justify-content start
+  align-items start
+  input[type="number"]
+    width 100%
+
+.form-text
+  align-items center
+  display flex
+  margin-top 10px
+  
+.settings-wrapper
+  z-index 999
+  width 100%
+  height 100vh
+  position fixed
+  background rgba(255, 255, 255, 0.6)
+  backdrop-filter blur(10px)
+  display flex
+  justify-content center
+  align-items center
+
 .clear
   display flex
   width 100%
--- src/script/generator.js
+++ src/script/generator.js
@@ -1,27 +1,51 @@
-export default function getRandom (count, max = 10) {
-  let text = ''
+function getOperation (operations = { minus: true, plus: true, times: true }) {
+  let operation = ''
+  operation = Math.floor((Math.random() * 5) + 1)
+  switch (operation) {
+    case 1:
+    case 2:
+      if (operations.plus) {
+        operation = '+'
+      } else {
+        operation = getOperation(operations)
+      }
+      break
+    case 3:
+    case 4:
+      if (operations.minus) {
+        operation = '-'
+      } else {
+        operation = getOperation(operations)
+      }
+      break
+    case 5:
+      if (operations.times) {
+        operation = '*'
+      } else {
+        operation = getOperation(operations)
+      }
+      break
+  }
+  return operation
+}
 
+export default function getRandom (
+  count,
+  max = 10,
+  min = 1,
+  operations = { minus: true, plus: true, times: true }
+) {
+  let text = ''
+  let operation = ''
   for (let i = 0; i < count; i++) {
     let coefficient = Math.floor(Math.random() * 2) === 1 ? 1 : -1
-    let opperation = ''
+
     if (i > 0) {
-      opperation = Math.floor((Math.random() * 5) + 1)
-      switch (opperation) {
-        case 1:
-        case 2:
-          opperation = '+'
-          break
-        case 3:
-        case 4:
-          opperation = '-'
-          break
-        case 5:
-          opperation = '*'
-          break
-      }
+      operation = getOperation(operations)
     }
-    let number = Math.floor((Math.random() * max) + 1) * coefficient
-    text += ' ' + opperation + ' ' + (number < 0 ? '(' + number + ')' : number)
+
+    let number = Math.floor((Math.random() * max) + min) * coefficient
+    text += ' ' + operation + ' ' + (number < 0 ? '(' + number + ')' : number)
   }
   return {
     text: text.trim().replace(/\*/g, '•'),
